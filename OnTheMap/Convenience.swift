46fbbb5a49eef2a90b8faaf786b44fd0d957c37d
Youngsun Paik <youngsunpaik@gmail.com>
2016-06-17 02:40:44 -0700

    refactored to combine session info and account info in one function

--- OnTheMap/Convenience.swift
+++ OnTheMap/Convenience.swift
@@ -13,28 +13,11 @@ extension Client {
     
     func authenticateWithViewController(hostViewController: UIViewController, completionHandlerForAuth: (success: Bool, errorString: NSError?) -> Void) {
         
-        getSession(email!, password: password!) { success, session, errorString in
+        getSessionInfo(email!, password: password!) { success, sessionID, userID, errorString in
             if success {
-                guard let sessionID = session[Client.JSONResponseKeys.ID] as? String else {
-                    print("Could not find key in '\(Client.JSONResponseKeys.ID)' in \(session)")
-                    return
-                }
                 self.sessionID = sessionID
-                
-                self.getAccount(self.email!, password: self.password!
-                    , completionHandlerForAccount: { (success, account, errorString) in
-                        if success {
-                            guard let userID = account[Client.JSONResponseKeys.Key] as? String else {
-                                print("Could not find key in '\(Client.JSONResponseKeys.Key)' in \(session)")
-                                return
-                            }
-                            self.userID = Int(userID)
-                            completionHandlerForAuth(success: success, errorString: errorString)
-                            
-                        } else {
-                            completionHandlerForAuth(success: success, errorString: errorString)
-                        }
-                })
+                self.userID = Int(userID)
+                completionHandlerForAuth(success: success, errorString: errorString)
             } else {
                 completionHandlerForAuth(success: success, errorString: errorString)
             }
@@ -43,40 +26,40 @@ extension Client {
     }
     
     
-    func getSession(email: String, password: String, completionHandlerForSession: (success: Bool, session: [String:AnyObject]!, errorString: NSError?) -> Void) {
+    func getSessionInfo(email: String, password: String, completionHandlerForSessionInfo: (success: Bool, sessionID: String!, userID: String!, errorString: NSError?) -> Void) {
         
         let jsonBody = "{\"udacity\": {\"username\": \"\(email)\", \"password\": \"\(password)\"}}"
         
         taskForUdacityPostMethod(jsonBody) { (result, error) in
             if let error = error {
-                completionHandlerForSession(success: false, session: nil, errorString: error)
+                completionHandlerForSessionInfo(success: false, sessionID: nil, userID
+                    : nil, errorString: error)
             } else {
-                if let session = result[Client.JSONResponseKeys.Session] as? [String:AnyObject] {
-                    completionHandlerForSession(success: true, session: session, errorString: nil)
-                } else {
-                    completionHandlerForSession(success: false, session: nil, errorString: NSError(domain: "getSession", code: 0, userInfo: [NSLocalizedDescriptionKey: "Could not parse session information"]))
+                guard let session = result[Client.JSONResponseKeys.Session] as? [String:AnyObject] else {
+                    completionHandlerForSessionInfo(success: false, sessionID: nil, userID
+                        : nil, errorString: NSError(domain: "getSession session", code: 0, userInfo: [NSLocalizedDescriptionKey: "Could not parse session information"]))
+                    return
                 }
+                
+                guard let sessionID = session[Client.JSONResponseKeys.ID] as? String else {
+                    completionHandlerForSessionInfo(success: false, sessionID: nil, userID: nil, errorString: NSError(domain: "getSession sessionID", code: 0, userInfo: [NSLocalizedDescriptionKey: "Could not parse sessionID"]))
+                    return
+                }
+                
+                guard let account = result[Client.JSONResponseKeys.Account] as? [String:AnyObject] else {
+                    completionHandlerForSessionInfo(success: false, sessionID: nil, userID: nil, errorString: NSError(domain: "getSession account", code: 0, userInfo: [NSLocalizedDescriptionKey: "Could not parse account information"]))
+                    return
+                }
+                
+                guard let userID = account[Client.JSONResponseKeys.Key] as? String else {
+                    completionHandlerForSessionInfo(success: false, sessionID: nil, userID: nil, errorString: NSError(domain: "getSession userID", code: 0, userInfo: [NSLocalizedDescriptionKey: "Could not parse userID"]))
+                    return
+                }
+                
+
+                completionHandlerForSessionInfo(success: true, sessionID: sessionID, userID: userID, errorString: nil)
             }
         }
-        
     }
-    
-    func getAccount(email: String, password: String, completionHandlerForAccount: (success: Bool, account: [String:AnyObject]!, errorString: NSError?) -> Void) {
-        
-        let jsonBody = "{\"udacity\": {\"username\": \"\(email)\", \"password\": \"\(password)\"}}"
-        
-        taskForUdacityPostMethod(jsonBody) { (result, error) in
-            if let error = error {
-                completionHandlerForAccount(success: false, account: nil, errorString: error)
-            } else {
-                if let account = result[Client.JSONResponseKeys.Account] as? [String:AnyObject] {
-                    completionHandlerForAccount(success: true, account: account, errorString: nil)
-                } else {
-                    completionHandlerForAccount(success: false, account: nil, errorString: NSError(domain: "getAccount", code: 0, userInfo: [NSLocalizedDescriptionKey: "Could not parse account information"]))
-                }
-            }
-        }
-    }
-    
     
 }
