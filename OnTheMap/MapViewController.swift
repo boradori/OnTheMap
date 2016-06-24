9ae88b874ad8f714dec1a1dde5397272b8b34045
Youngsun Paik <youngsunpaik@gmail.com>
2016-06-24 03:00:55 -0700

    Added MapViewController file

---
+++ OnTheMap/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  OnTheMap
+//
+//  Created by Youngsun Paik on 6/24/16.
+//  Copyright © 2016 Youngsun Paik. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    
+    
+    
+    
+    
+}
