d2b93baa386869223e08443f875c93a136ee324a
Youngsun Paik <youngsunpaik@gmail.com>
2016-07-07 01:17:53 -0700

    Reachability works with alert view when no internet connectivity

--- OnTheMap/AppDelegate.swift
+++ OnTheMap/AppDelegate.swift
@@ -24,48 +24,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(AppDelegate.reachabilityChanged(_:)), name: ReachabilityChangedNotification, object: nil)
-        
-        
+
         do {
             internetReach = try Reachability.reachabilityForInternetConnection()
         } catch {
             print("Error! No internet connection!")
         }
         
-        
-        // startNotifier always listens to status change such as switching over from wifi to 3G
-        do {
-            try internetReach?.startNotifier()
-        } catch {
-            print("Notifier could not start")
-        }
-        
-        if internetReach != nil {
-            self.statusChangedWithReachability(internetReach!)
-        }
-        
         return true
-    }
-    
-    func reachabilityChanged(notification: NSNotification) {
-        print("Reachability status changed")
-        reachability = notification.object as? Reachability
-        self.statusChangedWithReachability(reachability!)
     }
     
     
     func statusChangedWithReachability(currentReachabilityStatus: Reachability) {
         
-        var networkStatus: Reachability.NetworkStatus = currentReachabilityStatus.currentReachabilityStatus
-        var statusString: String = ""
+        let networkStatus: Reachability.NetworkStatus = currentReachabilityStatus.currentReachabilityStatus
         
         print("StatusValue: \(networkStatus.description)")
         print("StatusValue: \(networkStatus.hashValue)")
         
         if networkStatus.hashValue == Reachability.NetworkStatus.NotReachable.hashValue {
             print("Network not reachable")
+            
+            performUIUpdatesOnMain {
+                let alert = UIAlertController(title: "Alert", message: "Network not reachable", preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
+                self.window?.rootViewController?.presentViewController(alert, animated: true, completion: nil)
+            }
+            
             reachabilityStatus = kNOTREACHABLE
         } else if networkStatus.hashValue == Reachability.NetworkStatus.ReachableViaWiFi.hashValue {
             print("Reachable via WIFI")
@@ -96,7 +81,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        NSNotificationCenter.defaultCenter().removeObserver(self, name: ReachabilityChangedNotification, object: nil)
     }
 
 
